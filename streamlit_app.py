38248ebba2c25a9000599f1a3c508f8c51e56d31
urvashipriyam <upkumar@andrew.cmu.edu>
2021-12-06 18:07:05 -0500

    Add top player visualizations

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ from steam_dataset_app import SteamDatasetApp
 from covid_dataset_app import CovidDatasetApp
 from advanced_app import AdvancedApp
 from about_app import AboutApp
+from top_players_app import TopPlayersVizApp
 from location_viz import LocationVizApp
 
 
@@ -19,6 +20,7 @@ app = hy.HydraApp(title='Full Steam Ahead',
 app.add_app("Home", is_home=True, app=HomeApp())
 app.add_app("Steam Dataset", app=SteamDatasetApp())
 app.add_app("COVID-19 Dataset", app=CovidDatasetApp())
+app.add_app("Top Players", app=TopPlayersVizApp())
 app.add_app("Location Visualization", app=LocationVizApp())
 app.add_app("Advanced", app=AdvancedApp())
 app.add_app("About", app=AboutApp())
